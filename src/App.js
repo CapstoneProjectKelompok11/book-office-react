a1e2b1fbd2382850b66a95be6794da040269736e
Andhika Putra Permana <92287321+andhikapp6@users.noreply.github.com>
2022-06-09 23:24:05 +0700

    submit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./App.css";
-import Codeconfirm from "./components/Codeconfirm";
-import Login from "./components/Login";
+// import Codeconfirm from "./components/Codeconfirm";
+// import Login from "./components/Login";
 import Register from "./components/Register";
 
 function App() {
   return (
     <div>
-      <Login />
+      <Register />
     </div>
   );
 }
