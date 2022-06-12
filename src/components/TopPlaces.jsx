843de2bf0b52b920272020a46f11146279708fa9
achmadrizky486 <achmadrizky486@gmail.com>
2022-06-12 23:07:37 +0700

    fix : position card

--- src/components/TopPlaces.jsx
+++ src/components/TopPlaces.jsx
@@ -25,7 +25,8 @@ const TopPlaces = () => {
     <div className="mx-auto py-16 px-4 text-center">
       <h2 className="font-medium">Top Places in Jakarta</h2>
       <div className="justify-between pt-12">
-        <div className="grid grid-cols-3 gap-1 place-items-center ">
+        <div className="grid grid-cols-6 gap-4 items-center ">
+          <div className="flex col-span-1"></div>
           {data.map((builds) => {
             console.log(builds);
             return (
@@ -38,6 +39,7 @@ const TopPlaces = () => {
                       <img
                         src={`http://ec2-18-206-213-94.compute-1.amazonaws.com/api/building/image/${tes.fileName}`}
                         alt="img"
+                        className="h-full object-cover"
                       />
                     </>
                   );
@@ -45,6 +47,7 @@ const TopPlaces = () => {
               </>
             );
           })}
+          <div className="flex col-span-1"></div>
         </div>
       </div>
     </div>
