11ab2a3332c672383a19f749b93746646e6be8a4
Andhika Putra Permana <92287321+andhikapp6@users.noreply.github.com>
2022-06-12 20:09:45 +0700

    update data BE

--- src/components/TopPlaces.jsx
+++ src/components/TopPlaces.jsx
@@ -1,32 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import {dummyData} from './dummyData';
-import TopPlacesItem from "./TopPlacesItem";
+// import TopPlacesItem from "./TopPlacesItem";
 // import img from "../assets/Image.png";
 
-// console.log("dummyData", dummyData);
 const TopPlaces = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  
+  useEffect(() => {
+    axios
+    .get("http://ec2-18-206-213-94.compute-1.amazonaws.com/api/building?page=0&limit=10")
+    .then(res => {
+      setData(res.data.data);
+      console.log("res.data", res.data);
+        })
+        .catch(err => {
+          setError(err);
+        });
+    }, []);
+
   return (
     <div className="mx-auto py-16 px-4 text-center">
       <h2 className="font-medium">Top Places in Jakarta</h2>
       <div className="justify-between pt-12">
         <div className="grid grid-cols-3 gap-1 place-items-center ">
-           {dummyData.map((Datas) => {
-            return(
-              <TopPlacesItem 
-              key={Datas.id}
-              Name={Datas.Name}
-              Location={Datas.Location}
-              OfficeType={Datas.OfficeType}
-              Spesification={Datas.Spesification}
-              img={Datas.img}
-              />
-            )
-          })}   
+        {data.map((builds)=>(
+          <img src={builds.images}/>
+          // <p> {builds.name}</p>
+        ))}
+         
+          </div>
         </div>
-      </div>
-    </div>
-    
-    );
+      </div>    
+      );
+
 };
 
 
