263cbd0677c203a2fb794fe758811c0066875416
achmadrizky486 <achmadrizky486@gmail.com>
2022-06-13 09:52:30 +0700

    feat : get data builds.complex

--- src/components/Scbdhotel.jsx
+++ src/components/Scbdhotel.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import img from "../assets/kota.png";
 
 const Scbdhotel = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://ec2-18-206-213-94.compute-1.amazonaws.com/api/building?page=0&limit=10"
+      )
+      .then((res) => {
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
   return (
     <div>
-      <p className="ml-20 mt-40 text-2xl font-semibold">SCBD</p>
+      <p className="ml-20 mt-40 text-2xl font-semibold">Kuningan</p>
       <div className="grid grid-cols-4 gap-1 place-items-center pt-5 px-5">
-        <img className="w-[314px] h-[240px] object-cover" src={img} alt="/" />
-        <img className="w-[314px] h-[240px] object-cover" src={img} alt="/" />
-        <img className="w-[314px] h-[240px] object-cover" src={img} alt="/" />
-        <img className="w-[314px] h-[240px] object-cover" src={img} alt="/" />
+        {data.map((builds) => {
+          // console.log(builds);
+          return (
+            <>
+              {/* Karena "complex" itu object jadi cara mapnya seperti yg dibawah */}
+              {Object.keys(builds.complex).map((tes) => {
+                // {complexName}
+                console.log(tes.id);
+                return (
+                  <>
+                    <p>{tes.complexName}</p>
+                  </>
+                );
+              })}
+            </>
+          );
+        })}
       </div>
       <div>
         <p className="text-xl ml-20 mt-5 font-semibold underline underline-offset-4">
-          More Office in SCBD
+          More Office in Kuningan
         </p>
         <p className="ml-20 mt-24 text-2xl font-semibold">Senayan City</p>
         <div className="grid grid-cols-4 gap-1 place-items-center pt-5 px-5">
@@ -33,3 +62,24 @@ const Scbdhotel = () => {
 };
 
 export default Scbdhotel;
+// {
+//   data.map((builds) => {
+//     return (
+//       <>
+//         <p> {builds.complex.complexName}</p>
+//         {builds.complex.map((tes) => {
+//           console.log(tes.complexName);
+//           return (
+//             <>
+//               <img
+//                 src={`http://ec2-18-206-213-94.compute-1.amazonaws.com/api/building/image/${tes.fileName}`}
+//                 alt="img"
+//                 className="h-full object-cover"
+//               />
+//             </>
+//           );
+//         })}
+//       </>
+//     );
+//   });
+// }
