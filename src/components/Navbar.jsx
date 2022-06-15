7b4c9b4988e2a960f5c23a26e884f9e480117c64
achmadrizky486 <achmadrizky486@gmail.com>
2022-06-15 15:50:55 +0700

    fix : navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,24 +6,26 @@ import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
     <>
-      <div className="flex w-full justify-between items-center h-[60px] px-3 py-3 absolute z-10 text-white bg-[#4D89FF]">
-        <div className="flex items-center">
-          <div className="w-28 h-10 ml-20">
-            <img src={logo} alt="" />
+      <div>
+        <div className="flex w-full justify-between items-center h-[60px] px-3 py-3 text-white bg-[#4D89FF]">
+          <div className="flex items-center">
+            <div className="w-28 h-10 ml-20">
+              <img src={logo} alt="" />
+            </div>
           </div>
-        </div>
-        <div>
-          <ul className="flex">
-            <li className="mx-4">Home</li>
-            <li className="mx-4">Rent an Office</li>
-            <li className="mx-4">About Us</li>
-            <li className="mx-4">Find Us</li>
-          </ul>
-        </div>
-        <div className="flex">
-          <AiOutlineInbox size={18} />
-          <AiOutlineHeart size={18} className="mx-4" />
-          <VscAccount size={18} className="mr-20" />
+          <div>
+            <ul className="flex">
+              <li className="mx-4">Home</li>
+              <li className="mx-4">Rent an Office</li>
+              <li className="mx-4">About Us</li>
+              <li className="mx-4">Find Us</li>
+            </ul>
+          </div>
+          <div className="flex">
+            <AiOutlineInbox size={18} />
+            <AiOutlineHeart size={18} className="mx-4" />
+            <VscAccount size={18} className="mr-20" />
+          </div>
         </div>
       </div>
     </>
