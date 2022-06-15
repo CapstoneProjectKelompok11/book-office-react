ab6a43c4a12638378eca30b100ce40ebb0ba8f1b
achmadrizky486 <achmadrizky486@gmail.com>
2022-06-15 14:47:23 +0700

    feat : Ngejadiin page utuh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import DetailPage from "./components/DetailPage";
-import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
+import DetailPage from "./pages/DetailPage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route index element={<LandingPage />}></Route>
-        <Route path="/detail" element={<DetailPage />}></Route>
+        <Route path="/detail/:id" element={<DetailPage />}></Route>
       </Routes>
     </>
   );
