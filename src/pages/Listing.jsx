fad59ad4d01e295db1903e725251e14a4dc6052e
Steven <angsteven7@gmail.com>
2022-06-17 17:02:24 +0700

    feat: create Listing

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const Listing = ({item, hapus}) => {
+    return (
+        <tr>
+            {/* <td><p>{item.title}</p></td>
+            <td>
+                <button type="button" class="btn btn-danger" onClick={() => {hapus(item.id)}}>Delete</button>
+            </td> */}
+        </tr>
+    )
+}
