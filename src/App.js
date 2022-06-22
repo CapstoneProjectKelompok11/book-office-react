80e716d79addb4e7f0f3462a5e9e05e9175f07bc
Andhika Putra Permana <92287321+andhikapp6@users.noreply.github.com>
2022-06-23 00:04:03 +0700

    update routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 // import Codeconfirm from "./components/Codeconfirm";
-// import Login from "./components/Login";
+import Login from "./components/Login";
 import Register from "./components/Register";
 
 function App() {
   return (
-    <div>
-      <Register />
-      {/* <Login /> */}
-    </div>
+    <>
+     <Route path="/Login/" element={<Login />} />
+      <Route path="/Register/" element={<Register />} />
+
+    </>
   );
 }
 
