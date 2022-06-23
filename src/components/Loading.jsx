f79d1ad6bb6629d9697d79646b1bfa3676aa385e
achmadrizky486 <achmadrizky486@gmail.com>
2022-06-24 00:29:09 +0700

    feat : edit loading animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -29,6 +29,30 @@ const Loading = () => {
           <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
         </div>
       </div>
+      <div className="grid grid-cols-2 py-4 gap-8">
+        <div className="pl-5">
+          <Skeleton style={{ lineHeight: "70px" }} height={30} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={200} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={300} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={200} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={300} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+        </div>
+        <div className="pr-5">
+          <Skeleton style={{ lineHeight: "70px" }} height={30} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={200} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={300} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={200} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={300} />
+          <Skeleton style={{ lineHeight: "70px" }} height={30} width={500} />
+        </div>
+      </div>
     </div>
   );
 };
