f5a7764a576ccc1677520e1e1fca9696ba80dc2f
Andhika Putra Permana <92287321+andhikapp6@users.noreply.github.com>
2022-06-30 11:33:18 +0700

    update navigate

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -89,7 +89,7 @@ const Login = () => {
           setAllValid("valid");
           setLoading(false);
           console.log("response", response)
-          // navigate("/home");
+          navigate("/");
           Cookies.set("token", response.data.data.token);
         })
         .catch((error) => {
@@ -167,19 +167,7 @@ const Login = () => {
                       </svg>
                     </div>
                   </div>
-                  <div className="pt-5">
-                    <input
-                      type="checkbox"
-                      className="custom-checkbox w-5 h-5"
-                    />
-                    <label
-                      className="font-semibold text-gray-900 text-xl"
-                      for="vehicle1"
-                    >
-                      {" "}
-                      Remember Me
-                    </label>
-                  </div>
+                 
                   <div className="pt-5 text-center md:text-left">
                     <button
                     type="submit"
@@ -188,39 +176,7 @@ const Login = () => {
                       Login
                     </button>
                   </div>
-                  <div className="container-hr m-auto">
-                    <span className="span-hr">or</span>
-                  </div>
-                  <div className="relative pt-5 text-center md:text-left">
-                    <a
-                      href="#_"
-                      className="w-96 h-10 inline-block w-full pl-10 px-3 py-1 text-xl font-bold text-center text-gray-900 transition duration-200 bg-white border border-black rounded-lg hover:bg-gray-100 ease"
-                    >
-                      Login With Google
-                    </a>
-                    <div className="absolute mt-5 ml-20 inset-y-0 left-0 flex items-center px-2 pointer-events-none">
-                      <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M19.456 8.15401C19.579 8.81301 19.646 9.50201 19.646 10.221C19.646 15.845 15.882 19.844 10.197 19.844C8.90415 19.8444 7.6239 19.59 6.42939 19.0955C5.23489 18.6009 4.14954 17.8758 3.23536 16.9616C2.32118 16.0475 1.59609 14.9621 1.10152 13.7676C0.606957 12.5731 0.352603 11.2929 0.352997 10C0.352603 8.70716 0.606957 7.42691 1.10152 6.2324C1.59609 5.0379 2.32118 3.95255 3.23536 3.03837C4.14954 2.12419 5.23489 1.3991 6.42939 0.904533C7.6239 0.409966 8.90415 0.155612 10.197 0.156006C12.855 0.156006 15.076 1.13401 16.78 2.72201L14.005 5.49701V5.49001C12.972 4.50601 11.661 4.00101 10.197 4.00101C6.949 4.00101 4.309 6.74501 4.309 9.99401C4.309 13.242 6.949 15.992 10.197 15.992C13.144 15.992 15.15 14.307 15.562 11.993H10.197V8.15401H19.457H19.456Z"
-                          fill="black"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="pt-5 text-center md:text-left">
-                    <a
-                      href="#_"
-                      className="text-black underline font-semibold text-xl"
-                    >
-                      Forgot my password
-                    </a>
-                  </div>
+                
                   <div className="pt-2 text-center md:text-left">
                     <p className="w-full text-sm text-black text-xl">
                       Doesn’t have account?{" "}
